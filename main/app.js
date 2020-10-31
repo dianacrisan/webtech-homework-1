23db058aefafa2d4f7f9f521d6a1827219a79c17
Diana Crisan <crisanmonica18@stud.ase.ro>
2020-10-31 12:42:16 +0200

    modifiedHw1

--- main/app.js
+++ main/app.js
@@ -1,5 +1,32 @@
-function removeOrderItem(orderInfo, position){
-    
+function removeOrderItem(orderInfo, position) {
+    if(!orderInfo["items"] instanceof Array) {
+        throw new Error("Items should be an array");
+    }
+
+    let checkItem
+    for(let i=0; i<orderInfo.items.length; i++) {
+        checkItem = true
+        if(orderInfo.items[i].price == undefined || orderInfo.items[i].quantity == undefined) {
+            throw new Error('Malformed item');
+        }
+    }
+
+    if(position < 0 || position > orderInfo.items.length - 1) {
+        throw new Error("Malformed position");
+    }
+
+    let value = orderInfo.items[position].price * orderInfo.items[position].quantity;
+    orderInfo.total += value
+
+    let returnedOrderInfo 
+    let j = 0
+    for(let i=0; i<orderInfo.items.length; i++) {
+        if(i != position) {
+            returnedOrderInfo[j] = orderInfo.items[i]
+            j += 1
+        }
+    }
+    return returnedOrderInfo
 }
 
 const app = {
